7cb2f5abc777c0218986971754204d01949af2f6
Alejandro Baldominos <alexbaldo@gmail.com>
2016-02-26 16:16:35 +0100

    Added support for raw data DNN

--- pamap.py
+++ pamap.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 #
 # PAMAP.PY
 # Provides an interface for interacting with PAMAP2 dataset.
@@ -14,7 +15,7 @@ class PAMAP:
 	RAW = 0
 	PROCESSED = 1
 
-	NUM_SUBJECTS = 8
+	NUM_SUBJECTS = 2
 	CLASSES = {
 		1 : 0,		2 : 1,		3 : 2,		4 : 3,		5 : 4,		6 : 5,
 		7 : 6,		12 : 7,		13 : 8,		16 : 9,		17 : 10,	24 : 11	
@@ -33,11 +34,6 @@ class PAMAP:
 		else:
 			raise Exception( 'Invalid data source.' )
 
-	def filter_transitions ( self, f, k ):
-		for i, line in enumerate(f):
-			if line.split(' ')[k] != '0':
-				yield line
-
 	def load_raw ( self ):
 		base_path = os.path.dirname( __file__ )
 		data_path = os.path.join( base_path, 'data', 'pamap', 'raw' )
@@ -45,7 +41,7 @@ class PAMAP:
 		for subject in range( self.NUM_SUBJECTS ):
 			filename = os.path.join( data_path, 'subject10' + str(subject + 1) + '.dat' )
 			with open(filename) as f:
-				data = np.genfromtxt( self.filter_transitions( f, 1 ), delimiter = ' ', missing_values = '?', filling_values = 0 )
+				data = np.genfromtxt( f, delimiter = ' ' )
 			self.data.append([{ 
 				'features' : x[2:], 
 				'class' : self.CLASSES[x[1]]
@@ -64,6 +60,33 @@ class PAMAP:
 				'class' : self.CLASSES[x[-1]] 
 			} for x in data ])
 
+	def random_window ( self, dataset, size ):
+		# Stores the random window
+		window = []
+
+		# Extracts a random starting sample for the window.
+		start = np.random.randint( 0, len(dataset) )
+
+		# Determines the window class.
+		wclass = dataset[ start ][ 'class' ]
+
+		# Adds samples to the window until it is completely filled.
+		# The window must contain samples of only one class. If data
+		# from other class is reached, then the process will stop.
+		for i in range( start, min( start + size, len(dataset) ) ):
+			if dataset[ i ][ 'class' ] == wclass:
+				window.append( dataset[ i ] )
+
+		# In case the window is not complete (because another class is reached)
+		# Then it is filled from samples previous to the start sample.
+		# This should never reach another class, but if it does, then the
+		# window will be smaller than the specified size.
+		for i in reversed( range( max( 0, start - (size - len(window)) ), start ) ):
+			if dataset[ i ][ 'class' ] == wclass:
+				window.insert( 0, dataset[ i ] )
+
+		return window
+
 	def cross_validation ( self, fold ):
 		return { 
 			'train' : [ x for l in ( self.data[:fold-1] + self.data[fold:] ) for x in l ],
@@ -73,3 +96,9 @@ class PAMAP:
 	def random_sample ( self, dataset, sample_pct ):
 		return random.sample( dataset, int( sample_pct * len( dataset ) ) )
 
+	def random_window_batch ( self, dataset, window_size, batch_size ):
+		batch = []
+		while len( batch ) < batch_size:
+			batch.append( self.random_window( dataset, window_size ) )
+
+		return batch
---
+++ tf-pamap-raw-dnn.py
@@ -0,0 +1,149 @@
+# -*- coding: utf-8 -*-
+from pamap import PAMAP
+import tensorflow as tf
+import numpy as np
+
+# Defines the window size.
+W = 512
+
+# Converts a scalar to a one-hot encoding vector.
+def one_hot_encode ( range, value ):
+	vec = np.zeros( range )
+	vec[ value ] = 1
+	return vec
+
+# Initializes non-zero weights for a given shape.
+def weight_variable( shape ):
+  initial = tf.truncated_normal( shape, stddev=0.1 )
+  return tf.Variable( initial )
+
+# Initializes non-zero biases for a given shape.
+def bias_variable( shape ):
+  initial = tf.constant( 0.1, shape=shape )
+  return tf.Variable( initial )
+
+# Initializes a 2D convolution operator.
+def conv2d( x, W ):
+  return tf.nn.conv2d( x, W, strides=[1, 1, 1, 1], padding='SAME' )
+
+# Initializes a pooling operator.
+def max_pool( d1, d2, x ):
+  return tf.nn.max_pool( x, ksize=[1, d1, d2, 1], strides=[1, d1, d2, 1], padding='SAME' )
+
+# Loads the preprocessed and normalized PAMAP2 dataset.
+pamap = PAMAP( PAMAP.RAW )
+
+# Starts the TensorFlow interactive session.
+sess = tf.InteractiveSession()
+
+# Placeholders will be filled with data from the PAMAP dataset.
+# Shapes specify the dimensions of data inputs and outputs.
+x  = tf.placeholder( tf.float32, shape=[ None, W, 40 ] )		# Number of features: W * 40
+y_ = tf.placeholder( tf.float32, shape=[ None, 12  ] )  		# Number of classes: 12
+
+# Reshapes the input data to a more accurate representation
+# of the original data structure.
+# Width: 40 sensors
+# Height: 7 statistical measures
+# Depth: 1 channel
+x_repr = tf.reshape( x, [-1, W, 40, 1] )
+
+# L1 - CONVOLUTIONAL LAYER
+# 64x8 patch
+# 1 input channel
+# 1024 features
+W_conv1 = weight_variable([ 64, 8, 1, 1024 ])
+b_conv1 = bias_variable([ 1024 ])
+h_conv1 = tf.nn.relu( conv2d( x_repr, W_conv1 ) + b_conv1 )
+h_pool1 = max_pool( 64, 10, h_conv1 )
+
+# L2 - CONVOLUTIONAL LAYER
+# 4x2 patch
+# 1 input channel
+# 4096 features
+W_conv2 = weight_variable([ 4, 2, 1024, 4096 ])
+b_conv2 = bias_variable([ 4096 ])
+h_conv2 = tf.nn.relu( conv2d( h_pool1, W_conv2 ) + b_conv2 )
+h_pool2 = max_pool( 2, 1, h_conv2 )
+
+# L3 - DENSE LAYER
+# 4096 features
+# W * 40 neurons
+W_fc1 = weight_variable([ 4 * 4 * 4096, W * 40 ])
+b_fc1 = bias_variable([ W * 40 ])
+h_pool2_flat = tf.reshape( h_pool2, [-1, 4 * 4 * 4096] )
+h_fc1 = tf.nn.relu( tf.matmul( h_pool2_flat, W_fc1 ) + b_fc1 )
+
+# Dropouts
+keep_prob  = tf.placeholder( tf.float32 )
+h_fc1_drop = tf.nn.dropout( h_fc1, keep_prob )
+
+# LO - SOFTMAX LAYER
+W_fc2 = weight_variable([ W * 40, 12 ])
+b_fc2 = bias_variable([ 12 ])
+y = tf.nn.softmax( tf.matmul( h_fc1_drop, W_fc2 ) + b_fc2 )
+
+# Defines the cost function to be minimized.
+cost_function = -tf.reduce_mean( y_ * tf.log( y + 1E-10 ) )	# Cross entropy
+
+# Defines the optimization algorithm.
+# In this case, the chosen optimization algorithm is the 
+# Adam optimizer.
+#alpha = 0.001
+alpha = 0.0005
+train_step = tf.train.AdamOptimizer( alpha ).minimize( cost_function )
+
+# Defines the function that establishes whether a prediction is correct.
+# Also, defines the function used for measuring the classifier accuracy.
+correct_prediction = tf.equal( tf.argmax( y, 1 ), tf.argmax( y_, 1 ) )
+accuracy = tf.reduce_mean( tf.cast( correct_prediction, tf.float32 ) )
+
+# Stores the accuracies of training the models.
+accuracies = []
+
+# Iterates through all PAMAP2 subjects.
+for subject in range( 1, pamap.NUM_SUBJECTS + 1 ):
+
+	# Loads the LOSO-CV (leave-one-subject-out cross validation for
+	# one fold.
+	data  = pamap.cross_validation( subject )
+	train = data[ 'train' ]
+	test  = data[ 'test'  ]
+
+	# Reinitializes all the model variables.
+	sess.run( tf.initialize_all_variables() )
+
+	# Runs the training stage.
+	# In each iteration, only a random batch of the training
+	# set and the test set will be considered for efficiency purposes.
+	iters = 5000
+	for i in range( iters ):
+		train_batch = pamap.random_window_batch( train, W, 0.01 * len( train ) )
+		test_batch  = pamap.random_window_batch( test , W, 0.01 * len( test  ) )
+		print "Batches created!"
+		test_accuracy = accuracy.eval( feed_dict={
+			x  : [[ s[ 'features' ] for s in w ] for w in test_batch ],
+			y_ : [ one_hot_encode( 12, w[ 0 ][ 'class' ] ) for w in test_batch ],
+			keep_prob: 1.0
+		})
+   		print("step %d, test accuracy %g"%(i, test_accuracy))
+		
+
+		train_step.run( feed_dict = {
+			x  : [[ s[ 'features' ] for s in w ] for w in train_batch ],
+			y_ : [ one_hot_encode( 12, w[ 0 ][ 'class' ] ) for w in train_batch ],
+			keep_prob: 0.5
+		})
+
+	# Computes the accuracy of the classifier using the test set.
+	test_batch  = pamap.random_window_batch( test , W, 0.1 * len( test  ) )
+	model_accuracy = accuracy.eval( feed_dict = {
+		x  : [[ s[ 'features' ] for s in w ] for w in test_batch ],
+		y_ : [ one_hot_encode( 12, w[ 0 ][ 'class' ] ) for w in test_batch ],
+		keep_prob: 1.0
+	})
+
+	accuracies.append( model_accuracy )
+
+print accuracies
+print np.mean( accuracies )
