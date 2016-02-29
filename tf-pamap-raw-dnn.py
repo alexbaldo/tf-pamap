ea3835320e4f14a1801afea3f9ec4b453667b38a
baldo <alexbaldo@gmail.com>
2016-02-29 17:33:09 +0100

    Updated window size for TF RAW DNN

--- tf-pamap-raw-dnn.py
+++ tf-pamap-raw-dnn.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 import numpy as np
 
 # Defines the window size.
-W = 512
+W = 256
 
 # Converts a scalar to a one-hot encoding vector.
 def one_hot_encode ( range, value ):
@@ -52,26 +52,26 @@ x_repr = tf.reshape( x, [-1, W, 40, 1] )
 # 64x8 patch
 # 1 input channel
 # 1024 features
-W_conv1 = weight_variable([ 64, 8, 1, 1024 ])
-b_conv1 = bias_variable([ 1024 ])
+W_conv1 = weight_variable([ W/8, 8, 1, W*2 ])
+b_conv1 = bias_variable([ W*2 ])
 h_conv1 = tf.nn.relu( conv2d( x_repr, W_conv1 ) + b_conv1 )
-h_pool1 = max_pool( 64, 10, h_conv1 )
+h_pool1 = max_pool( W/8, 10, h_conv1 )
 
 # L2 - CONVOLUTIONAL LAYER
 # 4x2 patch
 # 1 input channel
 # 4096 features
-W_conv2 = weight_variable([ 4, 2, 1024, 4096 ])
-b_conv2 = bias_variable([ 4096 ])
+W_conv2 = weight_variable([ W/128, 2, W*2, W*4 ])
+b_conv2 = bias_variable([ W*4 ])
 h_conv2 = tf.nn.relu( conv2d( h_pool1, W_conv2 ) + b_conv2 )
 h_pool2 = max_pool( 2, 1, h_conv2 )
 
 # L3 - DENSE LAYER
 # 4096 features
 # W * 40 neurons
-W_fc1 = weight_variable([ 4 * 4 * 4096, W * 40 ])
+W_fc1 = weight_variable([ 4 * 4 * W * 4, W * 40 ])
 b_fc1 = bias_variable([ W * 40 ])
-h_pool2_flat = tf.reshape( h_pool2, [-1, 4 * 4 * 4096] )
+h_pool2_flat = tf.reshape( h_pool2, [-1, 4 * 4 * W * 4] )
 h_fc1 = tf.nn.relu( tf.matmul( h_pool2_flat, W_fc1 ) + b_fc1 )
 
 # Dropouts
@@ -79,7 +79,7 @@ keep_prob  = tf.placeholder( tf.float32 )
 h_fc1_drop = tf.nn.dropout( h_fc1, keep_prob )
 
 # LO - SOFTMAX LAYER
-W_fc2 = weight_variable([ W * 40, 12 ])
+W_fc2 = weight_variable([ W * 40, 12])
 b_fc2 = bias_variable([ 12 ])
 y = tf.nn.softmax( tf.matmul( h_fc1_drop, W_fc2 ) + b_fc2 )
 
@@ -90,7 +90,7 @@ cost_function = -tf.reduce_mean( y_ * tf.log( y + 1E-10 ) )	# Cross entropy
 # In this case, the chosen optimization algorithm is the 
 # Adam optimizer.
 #alpha = 0.001
-alpha = 0.0005
+alpha = 0.01
 train_step = tf.train.AdamOptimizer( alpha ).minimize( cost_function )
 
 # Defines the function that establishes whether a prediction is correct.
@@ -117,23 +117,26 @@ for subject in range( 1, pamap.NUM_SUBJECTS + 1 ):
 	# In each iteration, only a random batch of the training
 	# set and the test set will be considered for efficiency purposes.
 	iters = 10000
+        test_batch  = pamap.random_window_batch( test , W, 100 )
 	for i in range( iters ):
-		train_batch = pamap.random_window_batch( train, W, 700 )
-		test_batch  = pamap.random_window_batch( test , W, 100 )
-
+		train_batch = pamap.random_window_batch( train, W, 300 )
 		train_step.run( feed_dict = {
 			x  : [[ s[ 'features' ] for s in w ] for w in train_batch ],
 			y_ : [ one_hot_encode( 12, w[ 0 ][ 'class' ] ) for w in train_batch ],
 			keep_prob: 0.5
 		})
-
-		if i % 10:
-			test_accuracy = accuracy.eval( feed_dict={
-				x  : [[ s[ 'features' ] for s in w ] for w in test_batch ],
-				y_ : [ one_hot_encode( 12, w[ 0 ][ 'class' ] ) for w in test_batch ],
-				keep_prob: 1.0
-			})
-   			print("Subject %d - step %d, training accuracy %g"%(subject, i, test_accuracy))
+		print sess.run(W_fc1)
+		print sess.run(b_fc1)
+		print tf.argmax( y, 1 ).eval( feed_dict={
+                        x  : [[ s[ 'features' ] for s in w ] for w in test_batch ],
+			keep_prob: 1.0 
+		})
+		test_accuracy = accuracy.eval( feed_dict={
+			x  : [[ s[ 'features' ] for s in w ] for w in test_batch ],
+			y_ : [ one_hot_encode( 12, w[ 0 ][ 'class' ] ) for w in test_batch ],
+			keep_prob: 1.0
+		})
+   		print("Subject %d - step %d, training accuracy %g"%(subject, i, test_accuracy))
 		
 	# Computes the accuracy of the classifier using the test set.
 	test_batch  = pamap.random_window_batch( test , W, 0.1 * len( test  ) )
